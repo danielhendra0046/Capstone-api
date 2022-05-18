f49a37ec66de37fe2d26fb818e67a133ab7698bf
Daniel Hendra <danielhendra08@gmail.com>
2022-05-18 17:08:48 +0700

    Add POST and PUT

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const database = require('./database');
 const router = express();
+//const Validator = require('fastest-validator');
 
-
+// GET METHOD
 router.get('/', (req, res) => {
     res.send(database);
 });
 
+
+// POST METHOD
+router.post('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: 'menambahkan produk',
+    });
+});
+
+/*
+const v = new Validator();
+
+router.post('/', (req, res) => {
+    const schema = {
+        name: 'string',
+        status: 'string'
+    }
+
+    const validate = v.validate(req.body, schema);
+
+    if (validate.length) {
+        return res
+            .status(400)
+            .json(validate);
+    }
+
+    res.send('ok');
+});
+*/
+
+// PUT METHOD
+router.put('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: 'data berhasil diedit',
+    });
+});
 
 
 module.exports = router;
